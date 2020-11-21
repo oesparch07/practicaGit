39c833ee6b2dccadbd3b6eb504de096cf69ca153
Oriol <oriolesparch@gmail.com>
2020-11-21 20:42:42 +0100

    Codi classes Robot i Tresor

--- JocRobot/JocRobot/Robot.cs
+++ JocRobot/JocRobot/Robot.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace JocRobot
 {
     class Robot
     {
+        public double x, y;
+        public Ellipse robot = new Ellipse();
+        public Robot(double x , double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void posicioRobot()
+        {
+            robot.Width = robot.Height = 20;
+            robot.Fill = Brushes.Gray;
+            Canvas.SetLeft(robot, x);
+            Canvas.SetTop(robot, y);
+
+        }
     }
 }
--- JocRobot/JocRobot/Tresor.cs
+++ JocRobot/JocRobot/Tresor.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace JocRobot
 {
     class Tresor
     {
+        public double x, y;
+        public Rectangle tresor = new Rectangle();
+        public Tresor(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void posicioTresor()
+        {
+            tresor.Width = tresor.Height = 20;
+            tresor.Fill = Brushes.Gold;
+            Canvas.SetLeft(tresor, x);
+            Canvas.SetTop(tresor, y);
+
+        }
+
     }
 }
